284542c909a3a670ae4e871b3edeea7c1f6f0168
Daniel Ige <dantomige@gmail.com>
2025-09-26 00:17:42 -0400

    llm setup

---
+++ llm.py
@@ -0,0 +1,30 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+
+class Transformer(nn.Module):
+    
+    def __init__(self):
+        super().__init()
+        pass
+
+
+class LanguageModel(nn.Module):
+    
+    def __init__(self):
+        super().__init()
+        pass
+
+    def forward(self, X):
+        pass
+
+    def backward(self):
+        pass
+
+    def train(self, X, y):
+        pass
+
+
+if __name__ == "__main__":
+    pass
