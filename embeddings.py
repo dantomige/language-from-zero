53a4d3d02c850a36970d74de6a6f8809972eae68
Daniel Ige <dantomige@gmail.com>
2026-02-09 20:16:30 -0800

    style: rename for consistency

--- embeddings.py
+++ embeddings.py
@@ -4,18 +4,18 @@ import torch.nn as nn
 
 class EmbeddingLayer(nn.Module):
     
-    def __init__(self, vocab_size, d_model, max_seq_length):
+    def __init__(self, vocab_size, d_model, max_seq_len):
         super().__init__()
 
         self.vocab_size = vocab_size
         self.d_model = d_model
-        self.max_seq_length = max_seq_length
+        self.max_seq_len = max_seq_len
         self.word_embeddings = nn.Embedding(vocab_size, d_model)
-        self.positional_embeddings = self._generate_positional_embeddings(max_seq_length, d_model)
+        self.positional_embeddings = self._generate_positional_embeddings(max_seq_len, d_model)
 
-    def _generate_positional_embeddings(self, max_seq_length, d_model):
-        positional_embeddings = torch.zeros(max_seq_length, d_model)
-        positions = torch.arange(0, max_seq_length).unsqueeze(1)
+    def _generate_positional_embeddings(self, max_seq_len, d_model):
+        positional_embeddings = torch.zeros(max_seq_len, d_model)
+        positions = torch.arange(0, max_seq_len).unsqueeze(1)
         trig_arguments_column_factors = 10000 ** (torch.arange(0, d_model, 2)/d_model)
         # print(trig_arguments_column_factors)
         even_positional_embedding_values = torch.sin(positions / trig_arguments_column_factors)
@@ -36,14 +36,14 @@ if __name__ == "__main__":
 
     vocab_size = 5
     d_model = 4
-    max_seq_length = 10
+    max_seq_len = 10
 
     X = torch.tensor([0, 2, 4, 2, 1]).view(1, -1)
 
     # print(X.shape)
     # print(X)
 
-    embedding_layer = EmbeddingLayer(vocab_size, d_model, max_seq_length)
+    embedding_layer = EmbeddingLayer(vocab_size, d_model, max_seq_len)
 
     embeddings = embedding_layer(X)
 
