d43017439b4ec047881ab1b97488a0eb0d1e5d0c
Daniel Ige <dantomige@gmail.com>
2026-02-16 23:46:34 -0800

    bug: response to query

--- inference.py
+++ inference.py
@@ -5,46 +5,81 @@ from tokenizer import Tokenizer
 
 class Inference:
 
-    def __init__(self, model, tokenizer):
+    def __init__(self, model: Transformer, tokenizer: Tokenizer):
         self.model = model
         self.tokenizer = tokenizer
 
-    def response(self, query):
+    def response(self, query, max_response_tokens):
         ids = self.tokenizer.tokenize(query)
         ids_tensor = torch.tensor([ids])
-        ids_in_context_window = ids_tensor[: -self.model.context_window]
+        ids_in_context_window = ids_tensor[-self.model.context_window:]
 
         generated_response_token_ids = []
-        generated_token_id = None
-        end_token_id = tokenizer.tokenize("<END>")[0]
 
-        while generated_token_id != end_token_id:
+        for _iter in range(max_response_tokens):
+
             logits = self.model(ids_tensor)
             guesses = self.model.decode(logits)
 
             new_token_id = guesses[0, -1]
 
-            generated_response_token_ids.append(new_token_id)
-            generated_token_id = new_token_id
+            generated_response_token_ids.append(new_token_id.item())
+            
             ids_in_context_window = torch.cat(
                 [ids_in_context_window, torch.tensor([[new_token_id]])]
-            )
+            , dim=1)
 
-            if ids_in_context_window.shape > self.model.context_window:
-                ids_in_context_window = ids_in_context_window[1:]
+            _, curr_window_size = ids_in_context_window.shape
+            if  self.model.context_window < curr_window_size:
+                ids_in_context_window = ids_in_context_window[:,1:]
 
-        response = tokenizer.detokenize(generated_response_token_ids)
-        return response
+        response = self.tokenizer.detokenize(generated_response_token_ids)
+        return self.format(response)
 
-    def get_next_token(self, context_window):
+    # def response(self, query, max_response_length = None):
+    #     ids = self.tokenizer.tokenize(query)
+    #     ids_tensor = torch.tensor([ids])
+    #     ids_in_context_window = ids_tensor[: -self.model.context_window]
+
+    #     generated_response_token_ids = []
+    #     generated_token_id = None
+    #     end_token_id = self.tokenizer.get_token("<END>")
+
+    #     max_response_length = float("inf")
+
+    #     while generated_token_id != end_token_id and max_response_length:
+    #         logits = self.model(ids_tensor)
+    #         guesses = self.model.decode(logits)
+
+    #         new_token_id = guesses[0, -1]
+    #         print(new_token_id)
+
+    #         generated_response_token_ids.append(new_token_id)
+    #         generated_token_id = new_token_id
+
+    #         print(ids_in_context_window.shape)
+    #         ids_in_context_window = torch.cat(
+    #             [ids_in_context_window, torch.tensor([[new_token_id]])]
+    #         )
+
+    #         if ids_in_context_window.shape > self.model.context_window:
+    #             ids_in_context_window = ids_in_context_window[1:]
+
+    #         max_response_length -= 1
+
+    #     response = self.tokenizer.detokenize(generated_response_token_ids)
+    #     return self.format(response)
+
+    def generate_next_token(self, ids_tensor):
         pass
 
     def format(self, response):
-        return response
+        return " ".join(response)
 
 
 if __name__ == "__main__":
-    tokenizer = Tokenizer()
-    tokenizer.tokenize("What's up?")
-    model = Transformer()
-    inference = Inference()
+    # tokenizer = Tokenizer()
+    # tokenizer.tokenize("What's up?")
+    # model = Transformer()
+    # inference = Inference()
+    pass
