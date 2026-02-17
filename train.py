e1ed4d7d90203035c6e657c094ed3f55bfe27894
Daniel Ige <dantomige@gmail.com>
2026-02-16 17:55:59 -0800

    bug: fix tokenizer saving and allow for smaller training set

--- train.py
+++ train.py
@@ -1,20 +1,27 @@
 import torch
 import torch.nn as nn
+from utils import save_as_json
+from datetime import datetime, timezone
 from torch.utils.data import DataLoader
 from torch.optim import Adam
 from datasets import load_dataset
-
 from tokenizer import Tokenizer
 from llm import Transformer
 from src.dataset import LangaugeModelDataset
 
 
-def train_transformer(model, dataloader, optimizer, criterion, device):
+def train_transformer(model, dataloader, optimizer, criterion, device, num_datapoints_to_train = None):
 
     model.train()
     model.to(device)
 
+    num_examples_per_batch = dataloader.batch_size
+
     for batch_idx, (X, y) in enumerate(dataloader):
+        num_trained = num_examples_per_batch * batch_idx
+        if num_datapoints_to_train is not None and num_trained > num_datapoints_to_train:
+            break
+
         X, y = X.to(device), y.to(device)
         optimizer.zero_grad()
         yhat = model(X)
@@ -58,7 +65,7 @@ def main():
     tokenizer = Tokenizer()
     tokens = tokenizer.tokenize(full_text)
     tokens_tensor = torch.tensor(tokens)
-    vocab_size = tokenizer.vocab_size()
+    vocab_size = tokenizer.vocab_size
     print("Vocab size: ", vocab_size)
     print("Token tensor shape: ", tokens_tensor.shape)
 
@@ -66,6 +73,7 @@ def main():
     print("Creating dataset and dataloader objects")
     dataset = LangaugeModelDataset(tokens=tokens_tensor, context_window=context_window)
     dataloader = DataLoader(dataset=dataset, batch_size=batch_size, shuffle=True)
+    num_datapoints_to_train = 1000
     print("Dataloader length: ", len(dataloader))
 
     # create model
@@ -78,7 +86,7 @@ def main():
     )
 
     # select optimizer, criterion
-    print("Selecting optimizer and criteron")
+    print("Selecting optimizer and criterion")
     optimizer = Adam(model.parameters())
     criterion = nn.CrossEntropyLoss()
 
@@ -92,10 +100,25 @@ def main():
         optimizer=optimizer,
         criterion=criterion,
         device=device,
+        num_datapoints_to_train=num_datapoints_to_train
     )
 
     print("Saving model")
-    torch.save(model.state_dict(), "model_weights.pth")
+    utc_time_saved = datetime.now(timezone.utc)
+    tokenizer_filename = f"tokenizer_{utc_time_saved}"
+    tokenizer_state_dict = tokenizer.state_dict()
+    tokenizer = save_as_json(tokenizer_state_dict, tokenizer_filename)
+
+    checkpoint = {
+        "model_state_dict": model.state_dict(),
+        "vocab_size": vocab_size,
+        "d_model": d_model,
+        "n_blocks": n_blocks,
+        "context_window": context_window,
+        "utc_time": utc_time_saved,
+        "tokenizer_filename": tokenizer_filename,
+    }
+    torch.save(checkpoint, f"checkpoint_{utc_time_saved}.pth")
 
 
 if __name__ == "__main__":
