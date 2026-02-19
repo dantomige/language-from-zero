6ffc02df0bc379009d935d50d1495e41f77177f2
Daniel Ige <dantomige@gmail.com>
2026-02-19 05:38:09 -0800

    refactor: update io types

--- src/utils/io.py
+++ src/utils/io.py
@@ -1,11 +1,13 @@
 import json
+from pathlib import Path
+from typing import Union
 
-def save_json(data: dict, filepath: str):
+def save_json(data: dict, filepath: Union[str, Path]) -> None:
     """Saves a dictionary to a json file."""
     with open(filepath, "w") as f:
         json.dump(data, f, indent=4)
 
-def read_json(filepath: str) -> dict:
+def read_json(filepath: Union[str, Path]) -> dict:
     """Reads a json file and returns a dictionary."""
     with open(filepath, "r") as f:
         data = json.load(f)
