8f03b0beba5601a654bdfebfb84f414c5f875503
Daniel Ige <dantomige@gmail.com>
2026-02-19 14:35:50 -0800

    style: remove outdated type hinting

--- src/utils/io.py
+++ src/utils/io.py
@@ -1,13 +1,12 @@
 import json
 from pathlib import Path
-from typing import Union
 
-def save_json(data: dict, filepath: Union[str, Path]) -> None:
+def save_json(data: dict, filepath: str | Path) -> None:
     """Saves a dictionary to a json file."""
     with open(filepath, "w") as f:
         json.dump(data, f, indent=4)
 
-def read_json(filepath: Union[str, Path]) -> dict:
+def read_json(filepath: str | Path) -> dict:
     """Reads a json file and returns a dictionary."""
     with open(filepath, "r") as f:
         data = json.load(f)
