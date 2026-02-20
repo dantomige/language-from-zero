c6bc98298a514a55009609da402af27bc02d888a
Daniel Ige <dantomige@gmail.com>
2026-02-20 13:21:14 -0800

    refactor: improved inference model

--- inference.py
+++ inference.py
@@ -5,6 +5,7 @@ from src.model.llm import Transformer
 from src.tokenizer import Tokenizer
 from src.utils.checkpoint import CheckpointManager
 
+
 class Inference:
 
     def __init__(self, model: Transformer, tokenizer: Tokenizer):
@@ -12,8 +13,10 @@ class Inference:
         self.tokenizer = tokenizer
 
     @classmethod
-    def from_experiment(cls, checkpoint_dir: str | Path, experiment_folder_name: str) -> Inference:
-        """ Loads the model and tokenizer from a given experiment folder in the checkpoints directory and returns an Inference object.
+    def from_experiment(
+        cls, checkpoint_dir: str | Path, experiment_folder_name: str
+    ) -> Inference:
+        """Loads the model and tokenizer from a given experiment folder in the checkpoints directory and returns an Inference object.
 
         Args:
             checkpoint_dir (str | Path): path to checkpoints directory
@@ -26,11 +29,11 @@ class Inference:
         checkpoint_manager = CheckpointManager(checkpoint_dir=checkpoint_dir)
 
         bundle = checkpoint_manager.load(experiment_folder_name=experiment_folder_name)
-        
-        inference_model = cls(model = bundle.model, tokenizer=bundle.tokenizer)
+
+        inference_model = cls(model=bundle.model, tokenizer=bundle.tokenizer)
 
         return inference_model
-    
+
     def prepare_query(self, query: str) -> torch.Tensor:
         """Tokenizes the query and converts it to a tensor.
 
@@ -39,16 +42,35 @@ class Inference:
         Returns:
             torch.Tensor: tensor containing token ids of the query
         """
-        query_metadata_start_token_ids = [self.tokenizer.get_id(self.tokenizer.START_TOKEN), self.tokenizer.get_id(self.tokenizer.HEADER_START_TOKEN), self.tokenizer.get_id("user"), self.tokenizer.get_id(self.tokenizer.HEADER_END_TOKEN)]
+        query_metadata_start_token_ids = [
+            self.tokenizer.get_id(self.tokenizer.START_TOKEN),
+            self.tokenizer.get_id(self.tokenizer.HEADER_START_TOKEN),
+            self.tokenizer.get_id("user"),
+            self.tokenizer.get_id(self.tokenizer.HEADER_END_TOKEN),
+        ]
         query_token_ids = self.tokenizer.tokenize(query)
-        query_metadata_end_token_ids = [self.tokenizer.get_id(self.tokenizer.END_OF_TURN_TOKEN)]
+        query_metadata_end_token_ids = [
+            self.tokenizer.get_id(self.tokenizer.END_OF_TURN_TOKEN)
+        ]
+        assistant_response_metadata_token_ids = [
+            self.tokenizer.get_id(self.tokenizer.HEADER_START_TOKEN),
+            self.tokenizer.get_id("assistant"),
+            self.tokenizer.get_id(self.tokenizer.HEADER_END_TOKEN),
+        ]
 
-        full_query_token_ids = query_metadata_start_token_ids + query_token_ids + query_metadata_end_token_ids
+        full_query_token_ids = (
+            query_metadata_start_token_ids
+            + query_token_ids
+            + query_metadata_end_token_ids
+            + assistant_response_metadata_token_ids
+        )
         ids_tensor = torch.tensor([full_query_token_ids])
 
         return ids_tensor
 
-    def response(self, query: str, max_response_tokens: int, temperature: float = 1.0) -> str:
+    def response(
+        self, query: str, max_response_tokens: int, temperature: float = 1.0
+    ) -> str:
         """Given a query generates a response by predicting the next token until max_response_tokens is reached.
 
         Args:
@@ -59,77 +81,61 @@ class Inference:
         Returns:
             str: generated response
         """
-        ids = self.tokenizer.tokenize(query)
-        ids_tensor = torch.tensor([ids])
+        ids_tensor = self.prepare_query(query)
         ids_in_context_window = ids_tensor[-self.model.context_window :]
 
         generated_response_token_ids = []
 
-        for _iter in range(max_response_tokens):
+        num_tokens_generated = 0
+        last_generated_token_id = None
 
-            logits = self.model(ids_in_context_window) / temperature
+        while (
+            num_tokens_generated < max_response_tokens
+            and last_generated_token_id
+            != self.tokenizer.get_id(self.tokenizer.END_OF_TURN_TOKEN)
+        ):
 
-            guesses = self.model.decode(logits)
-
-            new_token_id = guesses[0, -1]
-
-            generated_response_token_ids.append(new_token_id.item())
-
-            ids_in_context_window = torch.cat(
-                [ids_in_context_window, torch.tensor([[new_token_id]])], dim=1
+            # generate the next token id
+            new_token_id = self.generate_next_token(
+                ids_in_context_window=ids_in_context_window, temperature=temperature
             )
 
-            _, curr_window_size = ids_in_context_window.shape
-            if self.model.context_window < curr_window_size:
-                ids_in_context_window = ids_in_context_window[:, 1:]
+            # update the context window with the new token id
+            ids_in_context_window = self.update_window(
+                ids_in_context_window=ids_in_context_window, new_token_id=new_token_id
+            )
 
-        response = self.tokenizer.detokenize(generated_response_token_ids)
-        return self.format(response)
+            # append the new token id to the generated response and update the context window
+            last_generated_token_id = new_token_id
+            num_tokens_generated += 1
+            generated_response_token_ids.append(new_token_id)
 
-    # def response(self, query, max_response_length = None):
-    #     ids = self.tokenizer.tokenize(query)
-    #     ids_tensor = torch.tensor([ids])
-    #     ids_in_context_window = ids_tensor[: -self.model.context_window]
+        return self.tokenizer.decode(generated_response_token_ids)
 
-    #     generated_response_token_ids = []
-    #     generated_token_id = None
-    #     end_token_id = self.tokenizer.get_token("<END>")
+    def generate_next_token(
+        self, ids_in_context_window: torch.Tensor, temperature: float = 1.0
+    ) -> int:
 
-    #     max_response_length = float("inf")
+        logits = self.model(ids_in_context_window) / temperature
+        guesses = self.model.decode(logits)
+        new_token_id = guesses[0, -1].item()
+        return new_token_id
 
-    #     while generated_token_id != end_token_id and max_response_length:
-    #         logits = self.model(ids_tensor)
-    #         guesses = self.model.decode(logits)
+    def update_window(
+        self, ids_in_context_window: torch.Tensor, new_token_id: int
+    ) -> torch.Tensor:
+        ids_in_context_window = torch.cat(
+            [ids_in_context_window, torch.tensor([[new_token_id]])], dim=1
+        )
+        _, curr_window_size = ids_in_context_window.shape
+        if self.model.context_window < curr_window_size:
+            ids_in_context_window = ids_in_context_window[:, 1:]
 
-    #         new_token_id = guesses[0, -1]
-    #         print(new_token_id)
+        return ids_in_context_window
 
-    #         generated_response_token_ids.append(new_token_id)
-    #         generated_token_id = new_token_id
-
-    #         print(ids_in_context_window.shape)
-    #         ids_in_context_window = torch.cat(
-    #             [ids_in_context_window, torch.tensor([[new_token_id]])]
-    #         )
-
-    #         if ids_in_context_window.shape > self.model.context_window:
-    #             ids_in_context_window = ids_in_context_window[1:]
-
-    #         max_response_length -= 1
-
-    #     response = self.tokenizer.detokenize(generated_response_token_ids)
-    #     return self.format(response)
-
-    def generate_next_token(self, ids_tensor: torch.Tensor, temperature: float = 1.0) -> int:
-        raise NotImplementedError
-    
     def format(self, response: list[str]) -> str:
         return " ".join(response)
 
 
 if __name__ == "__main__":
-    # tokenizer = Tokenizer()
-    # tokenizer.tokenize("What's up?")
-    # model = Transformer()
-    # inference = Inference()
     pass
